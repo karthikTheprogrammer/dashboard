b0e9b5e3327ea4af26c94940901630c93b9eed05
karthikTheprogrammer <skdkkarthik@gmail.com>
2026-05-18 00:04:20 +0530

    Final

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  output: 'export',
+  basePath: '/dashboard',
+  assetPrefix: '/dashboard/',
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
